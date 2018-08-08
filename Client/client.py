421a9d836f1bdf0b8c2b543b77d1b485564e49df
wangjiangchuan <wangjiangchuan@pset.suntec.net>
2018-08-08 15:37:51 +0800

    Revert "delete useless files"
    This reverts commit 8f8601caabd4619ef73b29e6cecacf93fada0ec9.

--- Client/client.py
+++ Client/client.py
@@ -39,23 +39,19 @@ class Client(object):
 
     def registure(self, name, pwd):
         buff = self._message.pack(requestcode.account, actioncode.registure, name + ' ' + pwd)
-        if buff:
-            self.senddata(buff)
+        self.senddata(buff)
 
     def updatepwd(self, name, pwd):
         buff = self._message.pack(requestcode.account, actioncode.updatepwd, name + ' ' + pwd)
-        if buff:
-            self.senddata(buff)
+        self.senddata(buff)
 
-    def login(self, name, pwd):
-        buff = self._message.pack(requestcode.logio, actioncode.login, name + ' ' + pwd)
-        if buff:
-            self.senddata(buff)
+    def login(self):
+        buff = self._message.pack(requestcode.logio, actioncode.login, '')
+        self.senddata(buff)
 
     def logout(self):
         buff = self._message.pack(requestcode.logio, actioncode.logout, '')
-        if buff:
-            self.senddata(buff)
+        self.senddata(buff)
 
     def senddata(self, buff):
         try:
--- Server/ORM.py
+++ Server/ORM.py
@@ -5,22 +5,22 @@ import logging; logging.basicConfig(level=logging.INFO)
 def log(sql, args=()):
     logging.info('SQL: {0}{1}'.format(sql, ' % {0}'.format(tuple(args)) if args else ''))
 
-# @asyncio.coroutine
-# def create_pool(loop, **kw):
-#     logging.info('create database connection pool...')
-#     global __pool
-#     __pool = yield from aiomysql.create_pool(
-#         host=kw.get('host', 'localhost'),
-#         port=kw.get('port', 3306),
-#         user=kw['user'],
-#         password=kw['password'],
-#         db=kw['database'],
-#         charset=kw.get('charset', 'utf8'),
-#         autocommit=kw.get('autocommit', True),
-#         maxsize=kw.get('maxsize', 10),
-#         minsize=kw.get('minsize', 1),
-#         loop=loop
-#     )
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['database'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+    )
 
 @asyncio.coroutine
 def select(sql, args, size=None):
